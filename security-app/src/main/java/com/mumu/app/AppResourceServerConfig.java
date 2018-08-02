0bf34e63f0a7a84e460246434dcb1d69b392b8d0
mumudong <850886448@qq.com>
2018-08-02 19:12:12 +0800

    qq

--- security-app/src/main/java/com/mumu/app/AppResourceServerConfig.java
+++ security-app/src/main/java/com/mumu/app/AppResourceServerConfig.java
@@ -1,12 +1,66 @@
 package com.mumu.app;
 
+import com.mumu.core.config.SmsCodeAuthenticationSecurityConfig;
+import com.mumu.core.config.ValidateCodeSecurityConfig;
+import com.mumu.core.properties.SecurityConstants;
+import com.mumu.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 /**
  * Created by Administrator on 2018/8/2.
  */
 @Configuration
 @EnableResourceServer
-public class AppResourceServerConfig {
+public class AppResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
+    @Autowired
+    private AuthenticationFailureHandler myAuthenticationFailureHandler;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+    @Autowired
+    private SpringSocialConfigurer mySocialSecurityConfig;
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(myAuthenticationFailureHandler);
+
+        http
+//                    .apply(validateCodeSecurityConfig)
+//                .and()
+                    .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                    .apply(mySocialSecurityConfig)
+                .and()
+                    .authorizeRequests()
+                     .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                    SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                    securityProperties.getBrowser().getLoginPage(),
+                    SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                    securityProperties.getBrowser().getSignUpUrl(),
+                    securityProperties.getBrowser().getSession().getSessionInvalidUrl() + ".html",
+                    securityProperties.getBrowser().getSession().getSessionInvalidUrl() + ".json",
+                    securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                    securityProperties.getBrowser().getSignOutUrl(),
+                    "/user/regist").permitAll()
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .csrf().disable();
+    }
 }
--- security-app/src/main/java/com/mumu/app/authentication/MyAuthenticationSuccessHandler.java
+++ security-app/src/main/java/com/mumu/app/authentication/MyAuthenticationSuccessHandler.java
@@ -3,10 +3,18 @@ package com.mumu.app.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mumu.core.properties.LoginResponseType;
 import com.mumu.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+import org.bouncycastle.util.encoders.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -22,14 +30,54 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     private ObjectMapper objectMapper;
     @Autowired
     private SecurityProperties securityProperties;
+
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         logger.info(String.format("登录成功 %s",""));
-        if(LoginResponseType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        }else{
-            super.onAuthenticationSuccess(request,response,authentication);
+
+        String header = request.getHeader("Authorization");
+        if(header == null || !header.startsWith("Basic ")){
+            throw new UnapprovedClientAuthenticationException("请求头中无client信息");
         }
+
+        String[] tokens = extractAndDecodeHeader(header,request);
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        if(clientDetails == null){
+            throw new UnapprovedClientAuthenticationException("clientId对应的配置信息不存在:" + clientId);
+        }else if(!StringUtils.equals(clientDetails.getClientSecret(),clientSecret)){
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配:" + clientId);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP,clientId,clientDetails.getScope(),"cusotm");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request,authentication);
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+    private String[] extractAndDecodeHeader(String header,HttpServletRequest request) throws IOException{
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try{
+            decoded = Base64.decode(base64Token);
+        }catch (IllegalArgumentException e){
+            throw new BadCredentialsException("Failed to decode basic authentication");
+        }
+        String token = new String(decoded,"UTF-8");
+        logger.info("token:{}",token);
+        int delimIndex = token.indexOf(":");
+        if(delimIndex == -1){
+            throw new BadCredentialsException("invalid basic authentication token");
+        }
+        return new String[] {token.substring(0,delimIndex),token.substring(delimIndex + 1)};
     }
 }
